669e62df0b1368aaa42cdf13df617343a93c57fc
samsunga50 <61355569+samsunga50@users.noreply.github.com>
2021-05-06 20:15:49 +0400

    Savings Added
    you can save money now...

--- cwa.py
+++ cwa.py
@@ -11,6 +11,7 @@ unit = int(input('CWA m3 used: '))
 UNIT = unit
 rate = 'MINIMUM_CHARGE'
 charge = MINIMUM_CHARGE
+savings = [0, 0]
 
 #FIRST10
 gamut = 10
@@ -20,6 +21,8 @@ if unit > gamut:
 else:
     rate = 'FIRST10'
     charge += unit * FIRST10
+    savings[0] = unit
+    savings[1] = unit * FIRST10
     
 #NEXT10 
 gamut = 10
@@ -29,6 +32,8 @@ if unit > gamut:
 elif rate == 'MINIMUM_CHARGE':
     rate = 'NEXT10'
     charge += unit * NEXT10
+    savings[0] = unit
+    savings[1] = unit * NEXT10
 
 #NEXT30  
 gamut = 30
@@ -38,10 +43,17 @@ if unit > gamut:
 elif rate == 'MINIMUM_CHARGE':
     rate = 'NEXT30'
     charge += unit * NEXT30
+    savings[0] = unit
+    savings[1] = unit * NEXT30
     
 #ADDITIONAL  
 if rate == 'MINIMUM_CHARGE':
     rate = 'ADDITIONAL'
     charge += unit * ADDITIONAL
+    savings[0] = unit
+    savings[1] = unit * ADDITIONAL
     
 print("Charge is Rs {} under the {} rate for {} units.".format(charge, rate, UNIT))
+
+if savings[0] < (UNIT / 2):
+    print("But... you could save Rs {} if you remove {} units off the cardboard...".format(savings[1], savings[0]))
